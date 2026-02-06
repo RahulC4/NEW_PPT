b4125372870c573f9ae4aecaf5becba08d246618
RahulC4 <101122855+RahulC4@users.noreply.github.com>
2026-02-06 14:25:39 +0530

    Create T15 app.py

---
+++ T15 app.py
@@ -0,0 +1,7 @@
+# frontend/app.py
+import streamlit as st
+
+st.set_page_config(page_title="AI PPT Generator", layout="wide")
+
+# Immediately redirect to Home page
+st.switch_page("frontend/pages/1_Home.py")
